d44dc45791db12031714fcdd752cbd0348879f97
jasonmorgan <jmorgan@f9vs.com>
2021-07-08 14:17:22 -0400

    updated init script to use demo-magic.

--- gitops/argo-rollouts/init.sh
+++ gitops/argo-rollouts/init.sh
@@ -4,16 +4,15 @@ clear
 k3d cluster delete rollouts &>/dev/null
 k3d cluster create rollouts -p "8000:80@loadbalancer" -s 3 &>/dev/null
 
-linkerd install | k apply -f - && linkerd check
-linkerd viz install | k apply -f - && linkerd viz check
-curl -sL https://run.linkerd.io/emojivoto.yml | linkerd inject - | kubectl apply -f -
+pe "linkerd install | k apply -f - && linkerd check"
+pe "linkerd viz install | k apply -f - && linkerd viz check"
 
-k get deploy -n kube-system traefik -o yaml | linkerd inject --skip-inbound-ports --ingress - | k apply -f -
+#curl -sL https://run.linkerd.io/emojivoto.yml | linkerd inject - | kubectl apply -f -
+
+pe "kubectl get deploy -n kube-system traefik -o yaml | linkerd inject --skip-inbound-ports --ingress - | kubectl apply -f -"
 
 
-kubectl create namespace argo-rollouts
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
+pe "kubectl create namespace argo-rollouts"
+pe "kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml"
 
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/smi/rollout.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/smi/services.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/smi/ingress.yaml
+pe "kubectl apply -f manifests/podinfo.yaml"
