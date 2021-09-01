297d407ebcd6c6d141a8ccdd9d5c8a9a2a0e02c4
jasonmorgan <jmorgan@f9vs.com>
2021-09-01 15:16:05 -0400

    meh.

---
+++ gitops/flux/flux.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+. ../../demo-magic.sh
+clear
+
+k3d cluster delete gitops &>/dev/null
+k3d cluster create gitops -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-server-arg '--no-deploy=traefik' > /dev/null 2>&1
+
+clear
+
+pe "flux check --pre"
+wait
+clear
+
+pe "flux install"
+wait
+clear
+
+# pe "kubectl get crd | grep flux"
+# wait
+# clear
+
+pe "linkerd check --pre"
+wait
+clear
+
+pe "bat -l yaml runtime/manifests/repo.yaml"
+wait
+clear
+
+pe "kubectl apply -f runtime/manifests/repo.yaml"
+wait
+clear
+
+pe "kubectl apply -f runtime/manifests/cluster.yaml"
+wait
+clear
+
+pe "bat -l yaml runtime/manifests/cluster.yaml"
+wait
+clear
+
+
+
+### THis looks good as it shows the live install
+
+pe "linkerd check"
+wait
+clear
+
+pe "linkerd viz check"
+wait
+clear
+
+pe "kubectl apply -f apps/manifests/apps.yaml"
+wait
+clear
+
+pe "bat -l yaml apps/manifests/apps.yaml"
+wait
+clear
