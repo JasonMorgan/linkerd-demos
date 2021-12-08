4eaf0e9b96b1433b6eb591e22d744985db2ad836
jasonmorgan <jmorgan@f9vs.com>
2021-12-08 12:33:20 -0500

    add gitops steps.

---
+++ gitops/weave_gitops_core/steps.sh
@@ -0,0 +1,33 @@
+# k3d cluster create gitops -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*'
+civo kubernetes create gitops -r Traefik -n 3 -s g3.k3s.small -w -y
+civo kubernetes config gitops > ~/.kube/config
+# k ctx gitops
+k ns default
+
+gitops install
+
+gitops add app --url https://github.com/JasonMorgan/linkerd-demos.git --path ./gitops/flux/runtime/manifests/ --name platform
+
+gh pr list
+
+gh pr merge $NUM
+
+k get host -A 
+
+gitops add app --url https://github.com/JasonMorgan/linkerd-demos.git --path ./gitops/flux/apps/source/podinfo --name podinfo --app-config-url NONE
+
+micro git_repos/jasonmorgan
+
+watch k get deploy
+
+## TAB2
+
+watch kubectl get pods -A
+
+watch kubectl get pods -n podinfo 
+
+## TAB3
+
+watch kubectl get kustomization
+
+watch linkerd viz stat ts
