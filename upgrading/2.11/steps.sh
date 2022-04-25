29602b4f75d9031c21dfbd8f84a5a738e7f327a6
jasonmorgan <jmorgan@f9vs.com>
2022-04-25 10:58:48 -0400

    Adding emissary.

--- upgrading/2.11/steps.sh
+++ upgrading/2.11/steps.sh
@@ -3,10 +3,11 @@ source ../../demo-magic.sh
 clear
 
 k3d cluster delete linkerd > /dev/null 2>&1 || true
-k3d cluster create linkerd -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-server-arg '--no-deploy=traefik'
+k3d cluster create linkerd --k3s-arg '--no-deploy=traefik@server:*;agents:*'
 curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
 k create ns booksapp
 curl -sL https://run.linkerd.io/booksapp.yml | kubectl apply -n booksapp -f -
+cd ~/tmp/ca
 wait 
 clear
 
@@ -18,7 +19,7 @@ pe "linkerd check"
 wait
 clear
 
-pe "helm install linkerd-dashboard linkerd/linkerd-viz"
+pe "helm install linkerd-dashboard linkerd/linkerd-viz --version 2.11.1"
 wait
 clear
 
@@ -34,11 +35,11 @@ pe "k get deploy -n emojivoto -o yaml | linkerd inject - | k apply -f -"
 wait
 clear
 
-pe "helm upgrade linkerd linkerd/linkerd2 --version 2.11.2"
+pe "helm upgrade linkerd linkerd/linkerd2 --version 2.11.2 -n default"
 wait
 clear
 
-pe "helm upgrade linkerd-dashboard linkerd/linkerd-viz --version 2.11.2"
+pe "helm upgrade linkerd-dashboard linkerd/linkerd-viz --version 2.11.2 -n default"
 wait
 clear
 
