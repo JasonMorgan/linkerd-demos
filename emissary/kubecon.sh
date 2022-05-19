7b69f62d0c85f56877b56796781194e2d4f4bb2b
jasonmorgan <jmorgan@f9vs.com>
2022-05-19 11:16:52 +0200

    latest demo.

---
+++ emissary/kubecon.sh
@@ -0,0 +1,93 @@
+#!/bin/env bash
+source ../demo-magic.sh
+# k3d cluster delete emissary > /dev/null 2>&1 || true
+k3d cluster create emissary -p "80:80@loadbalancer" -p "443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*' > /dev/null 2>&1 || true
+kubectl ns default
+curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
+
+clear
+
+
+## Install emissary
+pe "helm repo add datawire https://www.getambassador.io"
+wait
+clear
+
+pe "kubectl apply -f https://app.getambassador.io/yaml/emissary/2.2.2/emissary-crds.yaml"
+wait
+clear
+
+pe "kubectl wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system"
+wait
+clear
+
+pe "helm install -n emissary --create-namespace emissary-ingress datawire/emissary-ingress --set replicaCount=1 --wait"
+wait
+clear
+
+pe "kubectl create secret tls wildcard --cert ~/.certs/config/live/k8s.59s.io/cert.pem --key ~/.certs/config/live/k8s.59s.io/privkey.pem"
+wait
+clear
+
+pe "bat -l yaml manifests/podinfo/all-in-one.yaml"
+wait
+clear
+
+pe "kubectl apply -f manifests/podinfo/all-in-one.yaml"
+wait
+clear
+
+# pe "kubectl get ambassador-crds"
+# wait
+# clear
+
+## Install Linkerd
+pe "curl -sL https://run.linkerd.io/install | sh"
+wait
+clear
+
+pe "export PATH=\$PATH:\$HOME/.linkerd2/bin"
+clear
+
+pe "linkerd version"
+wait
+clear
+
+pe "linkerd check --pre"
+wait
+clear
+
+pe "linkerd install | kubectl apply -f - && linkerd check"
+wait
+clear
+
+pe "linkerd viz install | kubectl apply -f - && linkerd viz check"
+wait
+clear
+
+## Instegrate apps
+pe "kubectl get deploy -n emissary emissary-ingress -o yaml | linkerd inject - | kubectl apply -f -"
+wait
+clear
+
+pe "kubectl get deploy -n emojivoto -o yaml | linkerd inject - | kubectl apply -f -"
+wait
+clear
+
+pe "linkerd check -n emojivoto --proxy"
+wait
+clear
+
+p "fin"
+wait
+clear
+
+# Cleanup
+kubectl get deploy -n emojivoto -o yaml | linkerd uninject - | kubectl apply -f -
+kubectl get deploy -n emissary -o yaml | linkerd uninject - | kubectl apply -f - 
+kubectl wait --timeout=90s --for=condition=available deployment web -n emojivoto
+kubectl delete -f https://app.getambassador.io/yaml/emissary/2.2.2/emissary-crds.yaml
+kubectl delete secret wildcard
+linkerd viz uninstall | kubectl delete -f -
+linkerd uninstall | kubectl delete -f -
+helm delete  -n emissary emissary-ingress
