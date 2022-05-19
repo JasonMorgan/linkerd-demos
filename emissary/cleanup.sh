5458f08dd32723181fc35885ace544b3a0d9026c
jasonmorgan <jmorgan@f9vs.com>
2022-05-19 11:35:04 +0200

    hold your horses.

--- emissary/cleanup.sh
+++ emissary/cleanup.sh
@@ -3,7 +3,7 @@
 # Cleanup
 kubectl get deploy -n emojivoto -o yaml | linkerd uninject - | kubectl apply -f -
 kubectl get deploy -n emissary -o yaml | linkerd uninject - | kubectl apply -f - 
-kubectl wait --timeout=90s --for=condition=available deployment web -n emojivoto
+sleep 15
 kubectl delete -f https://app.getambassador.io/yaml/emissary/2.2.2/emissary-crds.yaml
 kubectl delete secret wildcard
 linkerd viz uninstall | kubectl delete -f -
