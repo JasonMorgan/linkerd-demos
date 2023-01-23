ab7e2647a38f8635eb8cff152dee7345e7c912d3
jasonmorgan <jmorgan@f9vs.com>
2023-01-23 16:38:40 -0500

    Linkerd update.
    Signed-off-by: jasonmorgan <jmorgan@f9vs.com>

--- gitops/flux/launch.sh
+++ gitops/flux/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-k3d cluster create gitops -s 3 -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*'
+k3d cluster create gitops -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*'
 flux install
 
 kubectl apply -f runtime/manifests/repo.yaml
