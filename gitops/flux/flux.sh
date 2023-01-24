cb8b5ed3baf6fa7f5014c4af7b48d37fb969feef
jasonmorgan <jmorgan@f9vs.com>
2023-01-24 10:49:59 -0500

    update install.
    Signed-off-by: jasonmorgan <jmorgan@f9vs.com>

--- gitops/flux/flux.sh
+++ gitops/flux/flux.sh
@@ -3,7 +3,7 @@
 clear
 
 k3d cluster delete gitops &>/dev/null
-k3d cluster create gitops -s 3 -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*' > /dev/null 2>&1
+k3d cluster create gitops -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"  --k3s-arg '--no-deploy=traefik@server:*;agents:*' > /dev/null 2>&1
 kubectl ns default
 
 clear
@@ -40,7 +40,9 @@ pe "bat -l yaml runtime/manifests/cluster.yaml"
 wait
 clear
 
-
+pe "linkerd check"
+wait
+clear
 
 ### This looks good as it shows the live install
 
